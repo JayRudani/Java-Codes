588789a3dfe1d6e5186f60b516e85927fb723fd6
Jay Rudani <jayrudani141414@gmail.com>
2022-02-03 11:50:25 -0500

    Create NumberPlateGenerator.java

---
+++ Collection 6/NumberPlateGenerator.java
@@ -0,0 +1,22 @@
+public class NumberPlateGenerator {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 3; i++) { // Initializing for loop and declaring and assigning variable i in it
+			double a = Math.random() * 26; // generating a random number and multiply it by 26 and assigning it to
+											// variable a
+			int b = (int) a + 65; // Adding 65 to the random number as upper-case letter are in between 65 to 90
+									// in ASCII table and assigning it to variable b
+			char c = (char) b; // converting the number to character
+			System.out.print(c); // printing the character
+		}
+		for (int i = 0; i <= 2; i++) { // Initializing for loop and declaring and assigning variable i in it
+			double a = Math.random() * 9; // generating a random number and multiply it by 9 and assigning it to
+											// variable a
+			int b = (int) a; // casting the variable a from double to integer and storing it into variable b
+			System.out.print(b); // printing the integer
+		}
+
+	}
+
+}
